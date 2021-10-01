7ba9d18b55327adef2d09062f073a2171404839e
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2021-10-01 13:21:27 -0400

    Initial Commit

---
+++ Blog/BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogApp.swift
+//  Blog
+//
+//  Created by Eduardo David De La Cruz Marrero on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
